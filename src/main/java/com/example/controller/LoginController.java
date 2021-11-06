804d7622779d8aa8a94fe4345d74e04a4fff432a
shibamirai <66295034+shibamirai@users.noreply.github.com>
2021-11-06 13:57:57 +0900

    7章 画面レイアウト

--- src/main/java/com/example/controller/LoginController.java
+++ src/main/java/com/example/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.example.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
@@ -9,5 +10,10 @@ public class LoginController {
 	@GetMapping("/login")
 	public String getLogin() {
 		return "login/login";
+	}
+
+	@PostMapping("/login")
+	public String postLogin() {
+		return "redirect:/user/list";
 	}
 }
