34fcd5fecc9b0e6a38cc663b67f8fe882126224c
shibamirai <66295034+shibamirai@users.noreply.github.com>
2021-11-06 11:40:44 +0900

    6章 バインド&バリデーション（入力チェック）

--- src/main/java/com/example/controller/SignupController.java
+++ src/main/java/com/example/controller/SignupController.java
@@ -6,29 +6,47 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.application.service.UserApplicationService;
+import com.example.form.GroupOrder;
+import com.example.form.SignupForm;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @RequestMapping("/user")
+@Slf4j
 public class SignupController {
 
 	@Autowired
 	private UserApplicationService userApplicationService;
 
 	@GetMapping("/signup")
-	public String getSignup(Model model, Locale locale) {
-		Map<String, Integer> genderMap = userApplicationService.getGenderMap(locale);
+	public String getSignup(Model model, Locale locale,
+			@ModelAttribute SignupForm form) {
+		Map<String, Integer> genderMap =
+				userApplicationService.getGenderMap(locale);
 		model.addAttribute("genderMap", genderMap);
 
 		return "user/signup";
 	}
 
 	@PostMapping("/signup")
-	public String postSingup() {
+	public String postSingup(Model model, Locale locale,
+			@ModelAttribute @Validated(GroupOrder.class) SignupForm form,
+			BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			return getSignup(model, locale, form);
+		}
+
+		log.info(form.toString());
+
 		return "redirect:/login";
 	}
 }
