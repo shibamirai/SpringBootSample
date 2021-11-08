d445c3834a355a5a33a2c6e5aa8a5a4d354d6f00
shibamirai <66295034+shibamirai@users.noreply.github.com>
2021-11-08 17:00:43 +0900

    10.2.2 コントローラークラス毎の例外処理

--- src/main/java/com/example/controller/SignupController.java
+++ src/main/java/com/example/controller/SignupController.java
@@ -5,10 +5,13 @@ import java.util.Map;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -64,5 +67,25 @@ public class SignupController {
 
 		// ログイン画面にリダイレクト
 		return "redirect:/login";
+	}
+
+	/** データベース関連の例外処理 */
+	@ExceptionHandler(DataAccessException.class)
+	public String dataAccessExceptionHandler(DataAccessException e, Model model) {
+
+		model.addAttribute("error", "");
+		model.addAttribute("message", "SignupControllerで例外が発生しました");
+		model.addAttribute("status", HttpStatus.INTERNAL_SERVER_ERROR);
+		return "error";
+	}
+
+	/** その他の例外処理 */
+	@ExceptionHandler(Exception.class)
+	public String exceptionHandler(DataAccessException e, Model model) {
+
+		model.addAttribute("error", "");
+		model.addAttribute("message", "SignupControllerで例外が発生しました");
+		model.addAttribute("status", HttpStatus.INTERNAL_SERVER_ERROR);
+		return "error";
 	}
 }
