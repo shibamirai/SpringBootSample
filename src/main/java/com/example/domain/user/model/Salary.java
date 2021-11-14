293231c6924ae4bad2918a1c51229815a62d99d6
shibamirai <66295034+shibamirai@users.noreply.github.com>
2021-11-14 11:46:14 +0900

    13章 Spring Data JPA

--- src/main/java/com/example/domain/user/model/Salary.java
+++ src/main/java/com/example/domain/user/model/Salary.java
@@ -1,10 +1,18 @@
 package com.example.domain.user.model;
 
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name = "t_salary")
 public class Salary {
-	private String userId;
-	private String yearMonth;
+//	private String userId;
+//	private String yearMonth;
+	@EmbeddedId
+	private SalaryKey salaryKey;
 	private Integer salary;
 }
